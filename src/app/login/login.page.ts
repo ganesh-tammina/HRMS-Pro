713979f4de6a7184da1caa20ab01beee56f33c26
ganesh siva <ganesh.meesala@tammina.com>
2025-08-11 17:46:25 +0530

    made changes

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -11,12 +11,25 @@ import { Router } from '@angular/router';
   imports: [IonContent, IonInput, IonButton, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
-
+  email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
+  showPassword: boolean = false;
+  currentYear: number = new Date().getFullYear();
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
   goToDashboard() {
     this.router.navigate(['/Home']);
+  }
+  onLogin() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Remember Me:', this.rememberMe);
+    // TODO: Implement authentication logic here
+  }
+  togglePassword() {
+    this.showPassword = !this.showPassword;
   }
 }
