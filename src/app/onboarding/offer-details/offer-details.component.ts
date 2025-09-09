f91b56a58d1fea4e2fd095e357e5f3a18219d830
ganesh siva <ganesh.meesala@tammina.com>
2025-09-09 20:43:38 +0530

    made changes with salary

--- src/app/onboarding/offer-details/offer-details.component.ts
+++ src/app/onboarding/offer-details/offer-details.component.ts
@@ -1,14 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { CreateOfferHeaderComponent } from '../create-offer-header/create-offer-header.component';
+import { HeaderComponent } from 'src/app/shared/header/header.component';
+import { Router } from '@angular/router';
+import { CandidateService } from 'src/app/services/pre-onboarding.service';
 
 @Component({
-  selector: 'app-offer-details',
+  selector: 'app-offer-letter',
+  standalone: true,
   templateUrl: './offer-details.component.html',
   styleUrls: ['./offer-details.component.scss'],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    CreateOfferHeaderComponent,
+    HeaderComponent
+  ]
 })
-export class OfferDetailsComponent  implements OnInit {
+export class OfferDetailsComponent implements OnInit {
 
-  constructor() { }
+  selectedOption: string = 'template'; // default
+  selectedTemplate: string = 'SVS';   // default
+  previewText: string = '';
 
-  ngOnInit() {}
+  uploadedFileName: string | null = null;
+  @Input() candidate: any = {};
+  router: any;
+  constructor(private route: Router, private fb: FormBuilder, private candidateService: CandidateService) { }
+  ngOnInit() {
+    this.updatePreview();
+  }
 
+  // Switch preview content based on selected template
+  updatePreview() {
+    if (this.selectedTemplate === 'SVS') {
+      this.previewText = `
+        Dear {{BasicInfo.FirstName}} {{BasicInfo.LastName}},
+        <br><br>
+        Welcome to <b>SVS Towers</b>!! <br><br>
+        It was a pleasure interacting with you during our hiring process and
+        we believe you would make a great asset to {{CompanyInfo.CompanyName}}.
+      `;
+    } else if (this.selectedTemplate === 'TechTammina') {
+      this.previewText = `
+        Dear {{BasicInfo.FirstName}} {{BasicInfo.LastName}},
+        <br><br>
+        Welcome to <b>Tech Tammina</b>!! <br><br>
+        We are excited to have you onboard and look forward to seeing the best of your capabilities.
+      `;
+    }
+  }
+
+  // Radio option change
+  onOptionChange(event: any) {
+    this.selectedOption = event.detail.value;
+  }
+
+  // Template dropdown change
+  onTemplateChange(event: any) {
+    this.selectedTemplate = event.detail.value;
+    this.updatePreview();
+  }
+
+  // File upload
+  onFileUpload(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.uploadedFileName = file.name;
+      this.previewText = `<b>Custom Offer Letter uploaded:</b> ${file.name}`;
+    }
+  }
 }
--- src/app/salary-staructure/salary-staructure.component.ts
+++ src/app/salary-staructure/salary-staructure.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { HeaderComponent } from '../shared/header/header.component';
 import { CreateOfferHeaderComponent } from '../onboarding/create-offer-header/create-offer-header.component';
 
@@ -19,23 +20,29 @@ import { CreateOfferHeaderComponent } from '../onboarding/create-offer-header/cr
   ],
 })
 export class salaryStaructureComponent implements OnInit {
-  annualSalary: number = 600000; // default
+  annualSalary: number = 600000;
   salaryStructure: any = {};
+  candidate: any = {};
+  salaryForm!: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router) {
+    const nav = this.router.getCurrentNavigation();
+    this.candidate = nav?.extras.state?.['candidate'] || {};
+    console.log('Candidate:', this.candidate);
+  }
 
   ngOnInit() {
     this.calculateSalary();
   }
 
   calculateSalary() {
-    const basic = this.annualSalary * 0.40; // 40%
-    const hra = this.annualSalary * 0.16; // 16%
+    const basic = this.annualSalary * 0.40;
+    const hra = this.annualSalary * 0.16;
     const medical = 15000;
     const transport = 19200;
     const special = this.annualSalary - (basic + hra + medical + transport);
 
-    const pfEmployer = basic * 0.12; // 12% of Basic
+    const pfEmployer = basic * 0.12;
     const pfEmployee = basic * 0.12;
 
     this.salaryStructure = {
@@ -53,5 +60,18 @@ export class salaryStaructureComponent implements OnInit {
 
   onViewSalary() {
     this.calculateSalary();
+  }
+
+  goToOfferDetails() {
+    // Save salary into candidate before navigating
+    this.candidate.salary = {
+      annualSalary: this.annualSalary,
+      salaryStructure: this.salaryStructure
+    };
+
+    this.router.navigate(
+      ['/OfferDetailsComponent', this.candidate.id, encodeURIComponent(this.candidate.personalDetails.FirstName)],
+      { state: { candidate: this.candidate } }
+    );
   }
 }
