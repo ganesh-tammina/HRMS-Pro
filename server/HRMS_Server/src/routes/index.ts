6ab8eb04120854bc895ea3a0f1639e301b9b4467
bipulsardar421 <bipulsardar421@gmail.com>
2025-09-29 18:33:51 +0530

    Leaves

--- server/HRMS_Server/src/routes/index.ts
+++ server/HRMS_Server/src/routes/index.ts
@@ -1,12 +1,12 @@
 // src/routes/candidates/index.ts
-import { Router } from "express";
-import postRouter from "./candidates/employeePostRoutes";
-import getRouter from "./candidates/EmployeeGetRoutes";
-import putRouter from "./candidates/EmployeePutRoutes";
-import deleteRouter from "./candidates/EmployeeDeleteRoutes";
-import attendancePostRouter from "./candidates/attendance/attandancePostRoutes";
-import attendanceGetRouter from "./candidates/attendance/attandanceGetRoutes";
-import employeeRouter from "./Employees/employeeRoutes"; // 👈 import
+import { Router } from 'express';
+import postRouter from './candidates/employeePostRoutes';
+import getRouter from './candidates/EmployeeGetRoutes';
+import putRouter from './candidates/EmployeePutRoutes';
+import deleteRouter from './candidates/EmployeeDeleteRoutes';
+import attendancePostRouter from './candidates/attendance/attandancePostRoutes';
+import attendanceGetRouter from './candidates/attendance/attandanceGetRoutes';
+import employeeRouter from './Employees/employeeRoutes';
 const router = Router();
 
 router.use(postRouter);
@@ -17,9 +17,4 @@ router.use(attendancePostRouter); // ✅ now mounted under /candidates
 router.use(attendanceGetRouter);
 router.use(employeeRouter);
 
-
-
-
 export default router;
-
-
