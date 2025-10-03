3364b3d3d35e4930a80e4029267d28368b53e547
ganesh-tammina <82147644+ganesh-tammina@users.noreply.github.com>
2025-10-03 11:49:00 +0530

    made changes

--- src/app/Administration/admin/admin.component.ts
+++ src/app/Administration/admin/admin.component.ts
@@ -2,19 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { LeaveModalComponent } from './leave-modal/leave-modal.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule]
+  imports: [CommonModule, IonicModule, FormsModule, LeaveModalComponent]
 })
 export class AdminComponent implements OnInit {
   selectedFile: File | null = null;
+  showModal = false;
+  leaveData: any = null;
   constructor(private http: HttpClient) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    const savedData = localStorage.getItem('leaveData');
+    if (savedData) {
+      this.leaveData = JSON.parse(savedData);
+    }
+  }
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
   }
@@ -39,5 +48,22 @@ export class AdminComponent implements OnInit {
     this.http.post("http://30.0.0.78:3562/upload-holidays", formData)
       .subscribe((res: any) => console.log(res), (err: any) => console.error(err));
   }
+  openModal() {
+    this.showModal = true;
+  }
 
+  handleSave(leaves: any) {
+    this.leaveData = leaves;
+    localStorage.setItem('leaveData', JSON.stringify(leaves));
+    
+    this.showModal = false;
+  }
+
+  handleClose() {
+    this.showModal = false;
+  }
+  deleteLeaves() {
+    this.leaveData = null;
+    localStorage.removeItem('leaveData');
+  }
 }
