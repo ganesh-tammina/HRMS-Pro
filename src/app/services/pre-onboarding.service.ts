974fb60a07133cd899d8d7bbd837aace64b3d56f
ganesh-tammina <82147644+ganesh-tammina@users.noreply.github.com>
2025-10-06 11:04:49 +0530

    made changes

--- src/app/services/pre-onboarding.service.ts
+++ src/app/services/pre-onboarding.service.ts
@@ -51,7 +51,7 @@ export interface Candidate {
 })
 export class CandidateService {
 
-  private api = "http://30.0.0.221:3562/";
+  private api = "http://30.0.0.78:3562/";
   private apiUrl = `${this.api}candidates/jd`;
   private adminUrl = "http://30.0.0.78:3562/1/admin";
   private offerUrl = `${this.api}candidates/offer-details`;
@@ -62,7 +62,7 @@ export class CandidateService {
   private newpassword = `${this.api}add-pwd`;
   private updatepassword = `${this.api}change-new-pwd`;
   private changeoldEmpwd = `${this.api}change-pwd`;
-  private offerStatusapi = "http://30.0.0.221:3562/offerstatus/status"
+  private offerStatusapi = "http://30.0.0.78:3562/offerstatus/status"
 
 
   private candidatesSubject = new BehaviorSubject<Candidate[]>([]);
