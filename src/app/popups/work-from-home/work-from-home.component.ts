5aa58749e376edb2495e5e8120c095cc7d2ef6b4
Praveena <praveena.noudu@tammina.com>
2025-10-15 11:10:55 +0530

    work from home UI

---
+++ src/app/popups/work-from-home/work-from-home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-from-home',
+  templateUrl: './work-from-home.component.html',
+  styleUrls: ['./work-from-home.component.scss'],
+})
+export class WorkFromHomeComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
